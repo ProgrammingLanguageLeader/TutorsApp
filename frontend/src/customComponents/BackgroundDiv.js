e273f3a061b0799951b87503d032314f1567a649
ProgrammingLanguageLeader <vip.shoroch@gmail.com>
2018-09-26 18:12:12 +0300

    I'm joking. That's the actual commit

--- frontend/src/customComponents/BackgroundDiv.js
+++ frontend/src/customComponents/BackgroundDiv.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 const BackgroundDiv = styled.div`
   ${props => props.image ? `background-image: url(${props.image})` : ""};
-  background-size: contain;
+  background-size: 100%;
+  background-repeat: no-repeat;
   height: 100vh;
 `;
 
