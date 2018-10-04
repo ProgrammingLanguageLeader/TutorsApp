54935c06e3090479486f1e2d7a2af8446a26f773
Fizmatishe <uch.nikitin@yandex.ru>
2018-10-05 02:36:39 +0300

    small fix in profile

--- frontend/src/containers/MainTutor.js
+++ frontend/src/containers/MainTutor.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { 
-	Epic, View, Panel, PanelHeader, Cell, Avatar, List, InfoRow, Tabbar, TabbarItem, Group, Div, Button
+	Epic, View, Panel, PanelHeader, Cell, Avatar, List, HeaderButton, Tabbar, TabbarItem, Group, Div, Button
 } from '@vkontakte/vkui';
 
 import Icon28Search from '@vkontakte/icons/dist/28/search';
 import Icon28Document from '@vkontakte/icons/dist/28/document';
 import Icon28User from '@vkontakte/icons/dist/28/user';
+import Icon24Home from '@vkontakte/icons/dist/24/home';
+import Icon24Education from '@vkontakte/icons/dist/24/education';
+import Icon24Mention from '@vkontakte/icons/dist/24/mention';
+import Icon24Recent from '@vkontakte/icons/dist/24/recent';
+import Icon24Info from '@vkontakte/icons/dist/24/info';
+import Icon28Write from '@vkontakte/icons/dist/28/write';
 
 class MainTutor extends React.Component {
 	constructor(props) {
@@ -29,7 +35,23 @@ class MainTutor extends React.Component {
 	render() {
 		return (
       <Epic activeStory={this.state.activeStory} tabbar={
-        <div style={{width: "0px", height: "0px"}}></div>
+        <Tabbar>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'search'}
+            data-story="search"
+          ><Icon28Search /></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'active'}
+            data-story="active"
+          ><Icon28Document /></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'profile_tutor'}
+            data-story="profile_tutor"
+          ><Icon28User /></TabbarItem>
+        </Tabbar>
       }>
         <View id="search" activePanel="search">
           <Panel id="search">
@@ -47,7 +69,7 @@ class MainTutor extends React.Component {
           </Panel>
         </View>
        
-        <View id="active" activePanel="search">
+        <View id="active" activePanel="active">
           <Panel id="search">
             <PanelHeader noShadow
             >
@@ -65,105 +87,50 @@ class MainTutor extends React.Component {
 
         <View id="profile_tutor" activePanel="profile_tutor">
           <Panel id="profile_tutor">
-            <PanelHeader noShadow>
+            <PanelHeader 
+              noShadow
+              left={<HeaderButton><Icon28Write /></HeaderButton>}
+              right={<HeaderButton><Icon28Write /></HeaderButton>}
+            >
               Профиль
             </PanelHeader>
             <List>
               <Cell
                 size="l"
-                description="Школьный учитель, Возраст: 20"
+                description="20 лет"
                 before={<Avatar src="https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg"/>}
               >
                 Артур Стамбульцян
-              </Cell>
-              <Cell>
-                <InfoRow title="Стаж">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="Образование">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="Адрес">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="e-mail">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="О себе">
-                  {/* From EditProfile */}
-                </InfoRow>
               </Cell>
               <Cell
-                size="l"
-                description="Школьный учитель, Возраст: 20"
-                before={<Avatar src="https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg"/>}
+                before={<Icon24Recent />}
               >
-                Артур Стамбульцян
-              </Cell>
-              <Cell>
-                <InfoRow title="Стаж">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="Образование">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="Адрес">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="e-mail">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="О себе">
-                  {/* From EditProfile */}
-                </InfoRow>
+                Стаж
+                {/* From EditProfile */}
               </Cell>
               <Cell
-                size="l"
-                description="Школьный учитель, Возраст: 20"
-                before={<Avatar src="https://pp.userapi.com/c841034/v841034569/3b8c1/pt3sOw_qhfg.jpg"/>}
+                before={<Icon24Education />}
               >
-                Артур Стамбульцян
+                Образование
+                {/* From EditProfile */}
               </Cell>
-              <Cell>
-                <InfoRow title="Стаж">
-                  {/* From EditProfile */}
-                </InfoRow>
+              <Cell
+                before={<Icon24Home />}
+              >
+                Адрес
+                {/* From EditProfile */}
               </Cell>
-              <Cell>
-                <InfoRow title="Образование">
-                  {/* From EditProfile */}
-                </InfoRow>
+              <Cell
+                before={<Icon24Mention />}
+              >
+                e-mail
+                {/* From EditProfile */}
               </Cell>
-              <Cell>
-                <InfoRow title="Адрес">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="e-mail">
-                  {/* From EditProfile */}
-                </InfoRow>
-              </Cell>
-              <Cell>
-                <InfoRow title="О себе">
-                  {/* From EditProfile */}
-                </InfoRow>
+              <Cell
+                before={<Icon24Info />}
+              >
+                О себе
+                {/* From EditProfile */}
               </Cell>
             </List>
           </Panel>
