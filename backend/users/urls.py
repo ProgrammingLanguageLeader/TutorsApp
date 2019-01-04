77796dab70b58177e7723b91a58a4105fe1608f1
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-01-04 23:42:24 +0300

    urls comments cleaning

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 
-# from users.views import CreateUserView, UpdateUserView, GetUserView, \
-#     DeactivateUserView, DeleteUserView
 from rest_framework.routers import DefaultRouter
 
 from users.views import UsersViewSet
@@ -12,31 +10,3 @@ router.register(r'', UsersViewSet)
 urlpatterns = [
     path('', include(router.urls))
 ]
-
-# urlpatterns = [
-#     path(
-#         'create/',
-#         CreateUserView.as_view(),
-#         name='users-create'
-#     ),
-#     path(
-#         'update/',
-#         UpdateUserView.as_view(),
-#         name='users-update'
-#     ),
-#     path(
-#         'get/',
-#         GetUserView.as_view(),
-#         name='users-get'
-#     ),
-#     path(
-#         'deactivate/',
-#         DeactivateUserView.as_view(),
-#         name='users-deactivate'
-#     ),
-#     path(
-#         'delete/',
-#         DeleteUserView.as_view(),
-#         name='users-delete'
-#     ),
-# ]
