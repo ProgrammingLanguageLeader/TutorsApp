ea4e31c0149c2acf904e0bdad73f693fea689b7c
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-01-12 06:41:46 +0300

    urls update for better presentation when generation docs using swagger

--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -5,13 +5,9 @@ from django.views import generic
 
 from rest_framework_swagger.views import get_swagger_view
 
-from backend_project.settings.base import DEBUG
-from backend_project.settings.static import MEDIA_URL, MEDIA_ROOT
+from backend_project.settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 
-swagger_schema_view = get_swagger_view(title='Pastebin API')
-
-api_urls = [
-    path('', swagger_schema_view),
+api_urlpatterns = [
     path('users/', include('users.urls')),
     path('vk_apps_users/', include('vk_apps_users.urls')),
     path('vacancies/', include('vacancies.urls')),
@@ -20,8 +16,15 @@ api_urls = [
     path('lessons/', include('lessons.urls')),
 ]
 
+swagger_schema_view = get_swagger_view(
+    title='Tutors app API',
+    patterns=api_urlpatterns,
+    url='/api/'
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest-auth/', include('rest_framework.urls')),
     path(
         '',
         generic.RedirectView.as_view(
@@ -29,8 +32,8 @@ urlpatterns = [
             permanent=False
         ),
     ),
-    path('api/', include(api_urls)),
-    path('rest-auth/', include('rest_framework.urls'))
+    path('api/', swagger_schema_view),
+    path('api/', include(api_urlpatterns)),
 ]
 
 if DEBUG:
