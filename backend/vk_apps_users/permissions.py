7fbd72eb87cf99ea5608f5356a58a02f521db27d
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-01-12 07:28:52 +0300

    vk_apps_user connect action and refactoring

--- backend/vk_apps_users/permissions.py
+++ backend/vk_apps_users/permissions.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+
 from rest_framework import permissions
 
 from vk_apps_users.authentication import VkAppsAuthBackend
@@ -15,3 +17,17 @@ class AuthenticatedCreateUserOrSelfOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user.id == obj.user.id
+
+
+class AuthenticatedUsingPasswordAndVKApps(permissions.BasePermission):
+    def has_permission(self, request, view):
+        is_authenticated_via_vk = VkAppsAuthBackend. \
+            check_vk_apps_authentication(request)
+        if not is_authenticated_via_vk:
+            return False
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(
+            username=username, password=password
+        )
+        return bool(user)
--- backend/vk_apps_users/views.py
+++ backend/vk_apps_users/views.py
@@ -1,19 +1,36 @@
 from random import choice
 from string import ascii_lowercase, digits
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import viewsets
+
+from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 
-from vk_apps_users.permissions import AuthenticatedCreateUserOrSelfOnly
+from vk_apps_users.permissions import AuthenticatedCreateUserOrSelfOnly, \
+    AuthenticatedUsingPasswordAndVKApps
 from vk_apps_users.serializers import VkAppsUserSerializer
 from vk_apps_users.models import VkAppsUser
 
 
-class VkAppsUsersViewSet(viewsets.ModelViewSet):
+class VkAppsUsersViewSet(mixins.CreateModelMixin,
+                         mixins.DestroyModelMixin,
+                         mixins.RetrieveModelMixin,
+                         viewsets.GenericViewSet):
+    """
+    create:
+        Creates user by VK Apps credentials with random username and
+        unusable password
+    destroy:
+        Deletes connection between VK ID and user
+    retrieve:
+        Returns the given user with VK ID
+    connect:
+        Creates connection between existing user and VK ID providing
+        an opportunity to authenticate via VK Apps
+    """
     UserModel = get_user_model()
     queryset = VkAppsUser.objects.all()
     serializer_class = VkAppsUserSerializer
@@ -49,14 +66,23 @@ class VkAppsUsersViewSet(viewsets.ModelViewSet):
         })
         if not serializer.is_valid():
             user.delete()
-            return Response(
-                status=HTTP_400_BAD_REQUEST,
-                data=serializer.errors
-            )
-        return VkAppsUser.objects.create(user=user, vk_id=vk_id)
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        vk_apps_user = VkAppsUser.objects.create(user=user, vk_id=vk_id)
+        return Response(VkAppsUserSerializer(vk_apps_user).data)
 
-    @action(detail=False, methods=['post'], name='Connect User',
-            url_path='connect_user')
-    def connect_user(self, request):
-        # TODO: implement this method
-        return Response('OK')
+    @action(detail=False, methods=['post'],
+            permission_classes=[AuthenticatedUsingPasswordAndVKApps],
+            name='Connect User')
+    def connect(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        vk_id = request.data.get('vk_user_id')
+        serializer = VkAppsUserSerializer(data={
+            'vk_id': vk_id,
+            'user': user.id,
+        })
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        vk_apps_user = VkAppsUser.objects.create(user=user, vk_id=vk_id)
+        return Response(VkAppsUserSerializer(vk_apps_user).data)
