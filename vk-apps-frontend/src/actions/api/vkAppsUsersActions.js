1948cbe55ff73c0904d6a041496210c237f95a7a
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-01-27 23:49:53 +0300

    vk apps users actions

---
+++ vk-apps-frontend/src/actions/api/vkAppsUsersActions.js
@@ -0,0 +1,101 @@
+import { vkAppsUsersConstants } from 'constants/api';
+import { makeApiRequest } from 'services/api';
+
+const createVkAppsUser = (options) => {
+  return async dispatch => {
+    dispatch({
+      type: vkAppsUsersConstants.CREATE_VK_APPS_USER_REQUEST,
+    });
+    return makeApiRequest('vk_apps_users/', 'post', options)
+      .then(
+        response => {
+          dispatch({
+            type: vkAppsUsersConstants.CREATE_VK_APPS_USER_SUCCESS,
+            payload: response,
+          })
+        },
+        errors => {
+          dispatch({
+            type: vkAppsUsersConstants.CREATE_VK_APPS_USER_FAILURE,
+            payload: errors,
+          });
+        }
+      )
+  };
+};
+
+const getVkAppsUser = (id, options) => {
+  return async dispatch => {
+    dispatch({
+      type: vkAppsUsersConstants.GET_VK_APPS_USER_REQUEST,
+    });
+    return makeApiRequest(`vk_apps_users/${id}/`, 'get', options)
+      .then(
+        response => {
+          dispatch({
+            type: vkAppsUsersConstants.GET_VK_APPS_USER_SUCCESS,
+            payload: response,
+          })
+        },
+        errors => {
+          dispatch({
+            type: vkAppsUsersConstants.GET_VK_APPS_USER_FAILURE,
+            payload: errors,
+          });
+        }
+      )
+  };
+};
+
+const deleteVkAppsUser = (id, options) => {
+  return async dispatch => {
+    dispatch({
+      type: vkAppsUsersConstants.DELETE_VK_APPS_USER_REQUEST,
+    });
+    return makeApiRequest(`vk_apps_users/${id}/`, 'delete', options)
+      .then(
+        response => {
+          dispatch({
+            type: vkAppsUsersConstants.DELETE_VK_APPS_USER_SUCCESS,
+            payload: response,
+          })
+        },
+        errors => {
+          dispatch({
+            type: vkAppsUsersConstants.DELETE_VK_APPS_USER_FAILURE,
+            payload: errors,
+          });
+        }
+      )
+  };
+};
+
+const connectVkAppsUser = (id, options) => {
+  return async dispatch => {
+    dispatch({
+      type: vkAppsUsersConstants.CONNECT_VK_APPS_USER_REQUEST,
+    });
+    return makeApiRequest(`vk_apps_users/connect/`, 'post', options)
+      .then(
+        response => {
+          dispatch({
+            type: vkAppsUsersConstants.CONNECT_VK_APPS_USER_SUCCESS,
+            payload: response,
+          })
+        },
+        errors => {
+          dispatch({
+            type: vkAppsUsersConstants.CONNECT_VK_APPS_USER_FAILURE,
+            payload: errors,
+          });
+        }
+      )
+  };
+};
+
+export const vkAppsUsersActions = {
+  createVkAppsUser,
+  getVkAppsUser,
+  deleteVkAppsUser,
+  connectVkAppsUser,
+};
