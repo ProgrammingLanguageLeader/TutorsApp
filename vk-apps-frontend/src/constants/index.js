0fb9ac3e103a4aed8399489c4ee795b765e461f8
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-02 05:43:21 +0300

    ROOT_URL constant

--- vk-apps-frontend/src/constants/index.js
+++ vk-apps-frontend/src/constants/index.js
@@ -1,4 +1,5 @@
 export const DEBUG = process.env.NODE_ENV === 'development';
+export const ROOT_URL = process.env.ROOT_URL;
 
 export * from './filterConstants';
 export * from './locationConstants';
