2aae32014997807b52835772f100bb3e746daf06
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-02 03:28:35 +0300

    store refactoring

--- vk-apps-frontend/src/store/store.js
+++ vk-apps-frontend/src/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import rootReducer from '../reducers';
+import rootReducer from 'vk-apps-frontend/reducers';
 
 const store = createStore(
   rootReducer,
