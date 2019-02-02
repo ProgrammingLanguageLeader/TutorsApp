c308a66c02d32313c0f29e47404b58391a04ab1d
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-02 03:27:55 +0300

    helpers refactoring

--- vk-apps-frontend/src/helpers/axiosMock.js
+++ vk-apps-frontend/src/helpers/axiosMock.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-import { API_URL } from "../constants";
+import { API_URL } from 'vk-apps-frontend/constants/api';
 
 const axiosInstance = axios.create();
 let mockAdapter = new MockAdapter(axiosInstance);
