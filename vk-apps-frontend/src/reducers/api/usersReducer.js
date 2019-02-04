71bd7a329766adb010659dd0cfeaab0fb6bb3d1b
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-04 04:54:05 +0300

    Users reducer with success flag

--- vk-apps-frontend/src/reducers/api/usersReducer.js
+++ vk-apps-frontend/src/reducers/api/usersReducer.js
@@ -10,6 +10,7 @@ const initialState = Immutable({
   usersPrevious: null,
   fetching: false,
   errors: null,
+  success: null,
 });
 
 const usersReducer = (state = initialState, action) => {
@@ -21,24 +22,29 @@ const usersReducer = (state = initialState, action) => {
     case usersConstants.DELETE_USER_REQUEST:
     case usersConstants.UPLOAD_AVATAR_REQUEST:
       return state.merge({
+        ...initialState,
         fetching: true,
       });
 
     case usersConstants.GET_USERS_LIST_SUCCESS:
       return state.merge({
+        ...initialState,
         users: action.payload.results,
         usersCount: action.payload.count,
         usersNext: action.payload.next,
         usersPrevious: action.payload.previous,
         fetching: false,
         errors: null,
+        success: true,
       });
 
     case usersConstants.GET_USER_SUCCESS:
       return state.merge({
+        ...initialState,
         user: action.payload,
         fetching: false,
         errors: null,
+        success: true,
       });
 
     case usersConstants.CREATE_USER_SUCCESS:
@@ -46,8 +52,10 @@ const usersReducer = (state = initialState, action) => {
     case usersConstants.DELETE_USER_SUCCESS:
     case usersConstants.UPLOAD_AVATAR_SUCCESS:
       return state.merge({
+        ...initialState,
         fetching: false,
         errors: null,
+        success: true,
       });
 
     case usersConstants.GET_USERS_LIST_FAILURE:
@@ -57,7 +65,8 @@ const usersReducer = (state = initialState, action) => {
     case usersConstants.DELETE_USER_FAILURE:
     case usersConstants.UPLOAD_AVATAR_FAILURE:
       return state.merge({
-        errors: action.payload,
+        ...initialState,
+        errors: action.payload.response.data,
         fetching: false,
       });
 
