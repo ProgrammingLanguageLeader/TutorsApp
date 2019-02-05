edd8a1bfa6988e7e86893d179baac09edb83bf2b
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-05 04:26:45 +0300

    Tabbar component

---
+++ vk-apps-frontend/src/components/Tabbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Tabbar from '@vkontakte/vkui/dist/components/Tabbar/Tabbar';
+import TabbarItem from '@vkontakte/vkui/dist/components/TabbarItem/TabbarItem';
+
+import Icon28Newsfeed from '@vkontakte/icons/dist/28/newsfeed';
+import Icon28Search from '@vkontakte/icons/dist/28/search';
+import Icon28User from '@vkontakte/icons/dist/28/user';
+import Icon28Notification from '@vkontakte/icons/dist/28/notification';
+import Icon28Menu from '@vkontakte/icons/dist/28/menu';
+
+export default ({
+  userId,
+  selectedItem,
+}) => (
+  <Tabbar>
+    <TabbarItem selected={selectedItem === 'user'}>
+      <Link to={`/user/${userId}/`}>
+        <Icon28User />
+      </Link>
+    </TabbarItem>
+
+    <TabbarItem selected={selectedItem === 'vacancies'}>
+      <Link to='/vacancies'>
+        <Icon28Search />
+      </Link>
+    </TabbarItem>
+
+    <TabbarItem selected={selectedItem === 'schedule'}>
+      <Link to='/schedule'>
+        <Icon28Newsfeed />
+      </Link>
+    </TabbarItem>
+
+    <TabbarItem selected={selectedItem === 'notifications'}>
+      <Link to='/notifications'>
+        <Icon28Notification />
+      </Link>
+    </TabbarItem>
+
+    <TabbarItem selected={selectedItem === 'menu'}>
+      <Link to='/menu'>
+        <Icon28Menu />
+      </Link>
+    </TabbarItem>
+  </Tabbar>
+);
