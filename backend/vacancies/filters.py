8928aae1b20d8df9641f30511d463c31ad7cae9d
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-05 15:50:34 +0300

    Vacancies filter upgrade

--- backend/vacancies/filters.py
+++ backend/vacancies/filters.py
@@ -1,9 +1,33 @@
-from rest_framework_filters import FilterSet
+import rest_framework_filters as filters
 
 from vacancies.models import Vacancy
 
 
-class VacancyFilter(FilterSet):
+class VacancyFilter(filters.FilterSet):
     class Meta:
         model = Vacancy
         fields = '__all__'
+
+    price__gte = filters.NumberFilter(
+        field_name='price',
+        lookup_expr='gte',
+        label='Price minimum'
+    )
+
+    price__lte = filters.NumberFilter(
+        field_name='price',
+        lookup_expr='lte',
+        label='Price maximum'
+    )
+
+    creation_time__gte = filters.DateTimeFilter(
+        field_name='creation_time',
+        lookup_expr='gte',
+        label='Creation time minimum'
+    )
+
+    creation_time__lte = filters.DateTimeFilter(
+        field_name='creation_time',
+        lookup_expr='lte',
+        label='Creation time maximum'
+    )
