4c6522dd8c87822634db81a5542975b411919366
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-08 00:00:09 +0300

    StudentRequest view implementation

--- vk-apps-frontend/src/views/StudentRequest.js
+++ vk-apps-frontend/src/views/StudentRequest.js
@@ -1,17 +1,67 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Moment from 'react-moment';
 
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 import Icon24Add from '@vkontakte/icons/dist/24/add';
 import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
 
 import BackIcon from 'vk-apps-frontend/components/BackIcon';
+import DivSpinner from 'vk-apps-frontend/components/DivSpinner';
+
+import { tutorsActions } from 'vk-apps-frontend/actions/api';
+
+import { ROOT_URL } from 'vk-apps-frontend/constants';
+
+const mapStateToProps = state => {
+  const { studentRequest, fetching } = state.apiReducer.tutorsReducer;
+  return {
+    studentRequest, fetching,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getStudentRequest: bindActionCreators(tutorsActions.getStudentRequest, dispatch),
+    acceptStudentRequest: bindActionCreators(tutorsActions.acceptStudentRequest, dispatch),
+    deleteStudentRequest: bindActionCreators(tutorsActions.deleteStudentRequest, dispatch),
+  };
+};
 
 class StudentRequest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAcceptButtonClick = this.handleAcceptButtonClick.bind(this);
+    this.handleDeclineButtonClick = this.handleDeclineButtonClick.bind(this);
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getStudentRequest(id);
+  }
+
+  async handleAcceptButtonClick() {
+    const { id } = this.props.match.params;
+    await this.props.acceptStudentRequest(id);
+    this.props.history.goBack();
+  }
+
+  async handleDeclineButtonClick() {
+    const { id } = this.props.match.params;
+    await this.props.deleteStudentRequest(id);
+    this.props.history.goBack();
+  }
+
   render() {
+    const { studentRequest, fetching } = this.props;
+
     return (
       <div>
         <PanelHeader left={
@@ -21,30 +71,81 @@ class StudentRequest extends React.Component {
         }>
           Заявка ученика
         </PanelHeader>
-        <Group title="Информация о заявке">
-          <div style={{ display: "flex", padding: 8 }}>
-            <Button
-              size="m"
-              level="primary"
-              before={<Icon24Add/>}
-              style={{ marginRight: 4 }}
-              onClick={() => console.log('Раньше здесь был мат')}
-            >
-              Принять
-            </Button>
-            <Button
-              size="m"
-              level="secondary"
-              before={<Icon24Cancel/>}
-              onClick={() => console.log('Раньше здесь был мат')}
-            >
-              Отклонить
-            </Button>
+
+        {fetching && (
+          <DivSpinner />
+        )}
+
+        {studentRequest && (
+          <div>
+            <Group title="Информация об ученике">
+              <Cell before={<Avatar size={64} src={ROOT_URL + studentRequest.student.avatar} />}>
+                {studentRequest.student.first_name} {studentRequest.student.last_name}
+              </Cell>
+
+              <Cell multiline description="Дата создания профиля">
+                <Moment locale="ru" format="D MMMM YYYY">
+                  {studentRequest.student.date_joined}
+                </Moment>
+              </Cell>
+
+              {studentRequest.student.city && (
+                <Cell description="Город">
+                  {studentRequest.student.city}
+                </Cell>
+              )}
+
+              {studentRequest.student.district && (
+                <Cell description="Район">
+                  {studentRequest.student.district}
+                </Cell>
+              )}
+
+              {studentRequest.student.street && (
+                <Cell description="Улица">
+                  {studentRequest.student.street}
+                </Cell>
+              )}
+
+              {studentRequest.student.metro_station && (
+                <Cell description="Станция метро">
+                  {studentRequest.student.metro_station}
+                </Cell>
+              )}
+
+              {studentRequest.student.bio && (
+                <Cell multiline description="О себе">
+                  {studentRequest.student.bio}
+                </Cell>
+              )}
+            </Group>
+
+            <Group title="Управление заявкой">
+              <div style={{ display: "flex", padding: 8 }}>
+                <Button
+                  size="m"
+                  level="primary"
+                  before={<Icon24Add/>}
+                  style={{ marginRight: 4 }}
+                  onClick={this.handleAcceptButtonClick}
+                >
+                  Принять
+                </Button>
+                <Button
+                  size="m"
+                  level="secondary"
+                  before={<Icon24Cancel/>}
+                  onClick={this.handleDeclineButtonClick}
+                >
+                  Отклонить
+                </Button>
+              </div>
+            </Group>
           </div>
-        </Group>
+        )}
       </div>
     );
   }
 }
 
-export default StudentRequest;
+export default connect(mapStateToProps, mapDispatchToProps)(StudentRequest);
