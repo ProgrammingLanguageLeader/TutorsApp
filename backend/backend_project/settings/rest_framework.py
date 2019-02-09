d85a482732c4dcecd8af415b6ef5585c07e48e5b
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-09 22:16:48 +0300

    Rest framework throttling

--- backend/backend_project/settings/rest_framework.py
+++ backend/backend_project/settings/rest_framework.py
@@ -2,17 +2,31 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'vk_apps_users.authentication.VkAppsAuthBackend',
     ),
+
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework_filters.backends.RestFrameworkFilterBackend',
     ),
+
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
+
     'PAGE_SIZE': 25,
 
-    'EXCEPTION_HANDLER': 'utils.exception_handler.custom_exception_handler'
+    'EXCEPTION_HANDLER': 'utils.exception_handler.custom_exception_handler',
+
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '45/minute'
+    }
 }
