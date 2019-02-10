0d9d3eb301ec509cdb91ef5197698a9f71e5ebe2
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-10 15:38:37 +0300

    PopoutDiv margin was added

--- vk-apps-frontend/src/components/PopoutDiv.js
+++ vk-apps-frontend/src/components/PopoutDiv.js
@@ -5,6 +5,7 @@ import Div from '@vkontakte/vkui/dist/components/Div/Div';
 const PopoutDiv = styled(Div)`
   background-color: rgba(255, 255, 255, 0.8);
   border-radius: 10px;
+  margin: 5px;
   text-align: center;
 `;
 
