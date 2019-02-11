7e72c7a3a50dfdfb4a3911f3138499809e28e59a
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-11 15:21:11 +0300

    SuccessfulFormStatus color fix

--- vk-apps-frontend/src/components/SuccessfulFormStatus.js
+++ vk-apps-frontend/src/components/SuccessfulFormStatus.js
@@ -3,12 +3,18 @@ import styled from 'styled-components';
 
 import FormStatus from '@vkontakte/vkui/dist/components/FormStatus/FormStatus';
 
-const SuccessfulFormStatus = styled(FormStatus).attrs({
-  state: "default"
-})`
+const SuccessfulDiv = styled.div`
+  border-radius: 10px;
+  padding: 12px;
   color: #155724;
   background-color: #d4edda;
   border-color: #c3e6cb;
 `;
+
+const SuccessfulFormStatus = ({ title }) => (
+  <SuccessfulDiv>
+    {title}
+  </SuccessfulDiv>
+);
 
 export default SuccessfulFormStatus;
