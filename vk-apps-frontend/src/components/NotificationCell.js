0222b6023567a10f59c99454cb70e00c4029ac17
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-15 02:25:36 +0300

    Notifications refactoring & showing labels accordingly to notification verb

---
+++ vk-apps-frontend/src/components/NotificationCell.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import styled from 'styled-components';
+import Moment from 'react-moment';
+
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+
+import { ROOT_URL } from 'vk-apps-frontend/constants';
+
+const FlexDiv = styled.div`
+  display: flex;
+`;
+
+const FlexDivWithPadding = styled(FlexDiv)`
+  padding-top: 8px;
+  padding-bottom: 8px;
+`;
+
+const GraySmallTextDiv = styled.div`
+  color: var(--text_secondary);
+  font-size: 14px;
+`;
+
+const NotificationCell = ({ notification, onSenderClick, buttonBefore, onButtonClick, buttonLabel }) => {
+  const { verb } = notification;
+  const notificationLabel =
+      verb === 'student request create' && 'Создана заявка на добавление в список учеников'
+      || verb === 'lesson create' && 'Создан урок'
+      || verb === 'student request accept' && 'Принята заявка на добавление в список учеников'
+      || verb === 'student request reject' && 'Отклонена заявка на добавление в список учеников'
+      || verb === 'lesson delete' && 'Удален урок';
+  const isExpandable = Boolean(notification.target);
+  return (
+    <Cell multiline expandable={isExpandable}>
+      <FlexDiv onClick={onSenderClick}>
+        <Avatar size={64} src={ROOT_URL + notification.sender.avatar} />
+        <Div>
+          <div>
+            {notification.sender.first_name} {notification.sender.last_name}
+          </div>
+          {isExpandable && (
+            <GraySmallTextDiv>
+              Нажмите для просмотра
+            </GraySmallTextDiv>
+          )}
+        </Div>
+      </FlexDiv>
+      <FlexDivWithPadding>
+        { notificationLabel }
+      </FlexDivWithPadding>
+      <FlexDivWithPadding>
+        <Button
+          size="m"
+          level="outline"
+          before={buttonBefore}
+          onClick={onButtonClick}
+        >
+          { buttonLabel }
+        </Button>
+      </FlexDivWithPadding>
+      <FlexDiv>
+        <GraySmallTextDiv>
+          <Moment format="LLLL" value={notification.creation_time} />
+        </GraySmallTextDiv>
+      </FlexDiv>
+    </Cell>
+  );
+};
+
+export default NotificationCell;
--- vk-apps-frontend/src/views/Notifications.js
+++ vk-apps-frontend/src/views/Notifications.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import Moment from 'react-moment';
 
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import List from '@vkontakte/vkui/dist/components/List/List';
 import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
-import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
-import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 
 import Icon24View from '@vkontakte/icons/dist/24/view';
 import Icon24Hide from '@vkontakte/icons/dist/24/hide';
 
+import NotificationCell from 'vk-apps-frontend/components/NotificationCell';
 import BackIcon from 'vk-apps-frontend/components/BackIcon';
 import DivSpinner from 'vk-apps-frontend/components/DivSpinner';
 
 import { notificationsActions, tutorsActions } from 'vk-apps-frontend/actions/api';
 
-import { ROOT_URL } from 'vk-apps-frontend/constants';
-
 const mapStateToProps = state => {
-  const { fetching, notifications } = state.apiReducer.notificationsReducer;
+  const { fetching, unreadNotifications, readNotifications } = state.apiReducer.notificationsReducer;
   const { currentUserReducer } = state;
   return {
-    notifications,
+    unreadNotifications,
+    readNotifications,
     fetching,
     currentUserReducer,
   };
@@ -34,7 +30,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    getNotificationsList: bindActionCreators(notificationsActions.getNotificationsList, dispatch),
+    getUnreadNotificationsList: bindActionCreators(notificationsActions.getUnreadNotificationsList, dispatch),
+    getReadNotificationsList: bindActionCreators(notificationsActions.getReadNotificationsList, dispatch),
     setUnreadNotification: bindActionCreators(notificationsActions.setUnreadNotification, dispatch),
     acceptStudentRequest: bindActionCreators(tutorsActions.acceptStudentRequest, dispatch),
     deleteStudentRequest: bindActionCreators(tutorsActions.deleteStudentRequest, dispatch),
@@ -49,7 +46,10 @@ class Notifications extends React.Component {
 
   componentDidMount() {
     if (this.props.currentUserReducer.user) {
-      this.props.getNotificationsList();
+      Promise.all([
+        this.props.getUnreadNotificationsList(),
+        this.props.getReadNotificationsList(),
+      ]);
     }
   }
 
@@ -57,17 +57,14 @@ class Notifications extends React.Component {
     await this.props.setUnreadNotification(id, {
       unread
     });
-    await this.props.getNotificationsList();
+    await Promise.all([
+      this.props.getUnreadNotificationsList(),
+      this.props.getReadNotificationsList(),
+    ]);
   }
 
   render () {
-    const { fetching, notifications } = this.props;
-    const unreadNotifications = notifications.filter(
-      notification => notification.unread && notification.target
-    );
-    const readNotifications = notifications.filter(
-      notification => !notification.unread && notification.target
-    );
+    const { fetching, unreadNotifications, readNotifications } = this.props;
 
     return (
       <div>
@@ -84,42 +81,24 @@ class Notifications extends React.Component {
             <DivSpinner />
           )}
           <List>
-            {unreadNotifications.map(notification => (
-              <Cell
-                multiline
-                expandable
+            { unreadNotifications.map(notification => (
+              <NotificationCell
                 key={notification.id}
-                description={
-                  <div>
-                    <div>
-                      {notification.target.content_type === 'student_request' && 'Заявка на добавление в список учеников'}
-                    </div>
-                    <div>
-                      <Moment format="LL" date={notification.creation_time} />
-                    </div>
-                  </div>
+                notification={notification}
+                buttonBefore={<Icon24Hide/>}
+                buttonLabel={"Пометить прочитанным"}
+                onButtonClick={
+                  () => this.handleSetUnreadNotification(notification.id, false)
                 }
-              >
-                <div
-                  style={{ display: "flex" }}
-                  onClick={() => this.props.history.push(`/${notification.target.content_type}/${notification.target.id}`)}
-                >
-                  <Avatar size={64} src={ROOT_URL + notification.sender.avatar} />
-                  <Div>
-                    {notification.sender.first_name} {notification.sender.last_name}
-                  </Div>
-                </div>
-                <div style={{ display: "flex", paddingTop: 8, paddingBottom: 8 }}>
-                  <Button size="m" level="outline" before={<Icon24Hide/>} onClick={
-                    () => this.handleSetUnreadNotification(notification.id, false)
-                  }>
-                    Пометить просмотренным
-                  </Button>
-                </div>
-              </Cell>
+                onSenderClick={
+                  notification.target
+                    ? () => this.props.history.push(`/${notification.target.content_type}/${notification.target.id}`)
+                    : null
+                }
+              />
             ))}
 
-            {unreadNotifications.length === 0 && (
+            { unreadNotifications.length === 0 && (
               <Div>Нет уведомлений</Div>
             )}
           </List>
@@ -127,39 +106,21 @@ class Notifications extends React.Component {
 
         <Group title="Просмотренные уведомления">
           <List>
-            {readNotifications.map(notification => (
-              <Cell
-                multiline
-                expandable
+            { readNotifications.map(notification => (
+              <NotificationCell
                 key={notification.id}
-                description={
-                  <div>
-                    <div>
-                      {notification.target.content_type === 'student_request' && 'Заявка на добавление в список учеников'}
-                    </div>
-                    <div>
-                      <Moment format="LL" date={notification.creation_time} />
-                    </div>
-                  </div>
+                notification={notification}
+                buttonBefore={<Icon24View/>}
+                buttonLabel={"Пометить непрочитанным"}
+                onButtonClick={
+                  () => this.handleSetUnreadNotification(notification.id, true)
                 }
-              >
-                <div
-                  style={{ display: "flex" }}
-                  onClick={() => this.props.history.push(`/${notification.target.content_type}/${notification.target.id}`)}
-                >
-                  <Avatar size={64} src={ROOT_URL + notification.sender.avatar} />
-                  <Div>
-                    {notification.sender.first_name} {notification.sender.last_name}
-                  </Div>
-                </div>
-                <div style={{ display: "flex", paddingTop: 8, paddingBottom: 8 }}>
-                  <Button size="m" level="outline" before={<Icon24View/>} onClick={
-                    () => this.handleSetUnreadNotification(notification.id, true)
-                  }>
-                    Пометить непросмотренным
-                  </Button>
-                </div>
-              </Cell>
+                onSenderClick={
+                  notification.target
+                    ? () => this.props.history.push(`/${notification.target.content_type}/${notification.target.id}`)
+                    : null
+                }
+              />
             ))}
 
             {readNotifications.length === 0 && (
