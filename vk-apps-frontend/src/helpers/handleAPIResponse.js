5eba5a3b783cecc08a02cf08025585a50cf84dbd
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-18 02:25:33 +0300

    status code error fix

--- vk-apps-frontend/src/helpers/handleAPIResponse.js
+++ vk-apps-frontend/src/helpers/handleAPIResponse.js
@@ -1,6 +1,6 @@
 export default (successAction, failureAction, dispatch) => response => {
-  const action = (response.status && response.status === 200) ? successAction : failureAction;
-  const payload = (response.status && response.status === 200) ? response.data : response;
+  const action = (response.status && response.status < 400) ? successAction : failureAction;
+  const payload = (response.status && response.status < 400) ? response.data : response;
   dispatch({
     type: action,
     payload: payload,
