9da9b8591fdc6d97e625cd0b4811610bd879e422
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-18 18:45:16 +0300

    internationalization settings

--- backend/backend_project/settings/internationalization.py
+++ backend/backend_project/settings/internationalization.py
@@ -1,5 +1,19 @@
-LANGUAGE_CODE = 'en-us'
+import os
+
+from backend_project.settings.base import BASE_DIR
+
+
+LANGUAGE_CODE = 'en'
+LANGUAGES = (
+  ('ru', 'Russian'),
+  ('en', 'English'),
+)
+
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
--- backend/backend_project/settings/middleware.py
+++ backend/backend_project/settings/middleware.py
@@ -8,4 +8,5 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
 ]
