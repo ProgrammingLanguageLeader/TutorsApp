cae292f3830fef1152107fbae62029a4747c17cb
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-20 05:14:38 +0300

    lesson beginning time validator fix

--- backend/lessons/validators.py
+++ backend/lessons/validators.py
@@ -7,14 +7,14 @@ from django.utils import timezone
 
 
 def validate_beginning_time(value):
-    now_datetime = timezone.now()
-    next_month_datetime = now_datetime + timedelta(days=30)
-    if value < now_datetime:
+    today_datetime = timezone.now() - timedelta(days=1)
+    next_month_datetime = timezone.now() + timedelta(days=30)
+    if value < today_datetime:
         raise ValidationError(
             _('Ensure this value is greater than '
               'or equal to %s.') % (
                 formats.date_format(
-                    now_datetime, "DATETIME_FORMAT", use_l10n=True
+                    today_datetime, "DATETIME_FORMAT", use_l10n=True
                 )
             )
         )
