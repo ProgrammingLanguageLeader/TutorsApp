288b4203f4f43c556c76344b0081dd8c429a8413
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-02-23 02:19:14 +0300

    vk_apps_users translation

--- backend/vk_apps_users/apps.py
+++ backend/vk_apps_users/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class VkAppsUsersConfig(AppConfig):
     name = 'vk_apps_users'
-    verbose_name = 'VK Apps Users'
+    verbose_name = _('VK Apps Users')
--- backend/vk_apps_users/models.py
+++ backend/vk_apps_users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 
 class VkAppsUser(models.Model):
@@ -8,16 +9,17 @@ class VkAppsUser(models.Model):
         null=False,
         blank=False,
         primary_key=True,
-        verbose_name='VK ID'
+        verbose_name=_('VK ID')
     )
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name=_('user')
     )
 
     class Meta:
-        verbose_name = 'VK Apps User'
-        verbose_name_plural = 'VK Apps Users'
+        verbose_name = _('VK Apps User')
+        verbose_name_plural = _('VK Apps Users')
 
     def __str__(self):
         return str(self.user)
