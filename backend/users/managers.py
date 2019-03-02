de528b6004d043ee02b3c6a4b98575c14746c5d3
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-02 22:14:38 +0300

    createsuperuser command fix

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -30,6 +30,9 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, username, email, password, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
         return self._create_user(username, email, password, **extra_fields)
