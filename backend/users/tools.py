51ced5c9c9c71e4a99ff5eebdd37329f4fef484c
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-05 03:01:46 +0300

    centering compressed avatar

--- backend/users/tools.py
+++ backend/users/tools.py
@@ -6,13 +6,23 @@ from PIL import Image
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
-def compress_avatar(avatar):
+def compress_avatar(avatar, width=200, height=200):
     avatar.file.open()
     avatar_image = Image.open(avatar.file)
-    avatar_image.thumbnail((200, 200))
+    avatar_image.thumbnail((width, height))
     avatar_format = avatar_image.format
+    avatar_mode = avatar_image.mode
+    centered_avatar_image = Image.new(avatar_mode, (width, height))
+    centered_avatar_image.paste(
+        avatar_image,
+        (
+            (width - avatar_image.size[0]) // 2,
+            (height - avatar_image.size[1]) // 2
+        )
+    )
+
     output = BytesIO()
-    avatar_image.save(output, format=avatar_format, quality=100)
+    centered_avatar_image.save(output, format=avatar_format, quality=100)
     output.seek(0)
     return InMemoryUploadedFile(
         output,
