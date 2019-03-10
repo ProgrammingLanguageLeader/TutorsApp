840a39e2d6797d3ed7cb884c3bcdedbcc9fdcbcf
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-10 21:01:26 +0300

    Notifications refactoring

--- vk-apps-frontend/src/views/Notifications.js
+++ vk-apps-frontend/src/views/Notifications.js
@@ -9,6 +9,7 @@ import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import List from '@vkontakte/vkui/dist/components/List/List';
 import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import PullToRefresh from '@vkontakte/vkui/dist/components/PullToRefresh/PullToRefresh';
 
 import Icon24View from '@vkontakte/icons/dist/24/view';
 import Icon24Hide from '@vkontakte/icons/dist/24/hide';
@@ -18,15 +19,6 @@ import BackIcon from 'vk-apps-frontend/components/BackIcon';
 import DivSpinner from 'vk-apps-frontend/components/DivSpinner';
 
 import { notificationsActions, tutorsActions } from 'vk-apps-frontend/actions/api';
-
-const mapStateToProps = state => {
-  const { fetching, unreadNotifications, readNotifications } = state.API.notificationsReducer;
-  return {
-    unreadNotifications,
-    readNotifications,
-    fetching,
-  };
-};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -41,28 +33,68 @@ const mapDispatchToProps = dispatch => {
 class Notifications extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      refreshing: false,
+      fetchingUnreadNotifications: false,
+      fetchingReadNotifications: false,
+      unreadNotifications: [],
+      readNotifications: [],
+    };
+    this.fetchNotifications = this.fetchNotifications.bind(this);
     this.handleSetUnreadNotification = this.handleSetUnreadNotification.bind(this);
+    this.onRefresh = this.onRefresh.bind(this);
   }
 
-  componentDidMount() {
-    Promise.all([
-      this.props.getUnreadNotificationsList(),
-      this.props.getReadNotificationsList(),
-    ]);
+  async fetchNotifications() {
+    this.setState({
+      fetchingUnreadNotifications: true,
+      fetchingReadNotifications: true,
+    });
+    const unreadNotificationsResponse = await this.props.getUnreadNotificationsList();
+    const readNotificationsResponse = await this.props.getReadNotificationsList();
+    const unreadNotifications = unreadNotificationsResponse.status === 200
+      ? unreadNotificationsResponse.data.results
+      : [];
+    const readNotifications = readNotificationsResponse.status === 200
+      ? readNotificationsResponse.data.results
+      : [];
+    this.setState({
+      fetchingReadNotifications: false,
+      fetchingUnreadNotifications: false,
+      unreadNotifications,
+      readNotifications,
+    });
+  }
+
+  async componentDidMount() {
+    await this.fetchNotifications();
   }
 
   async handleSetUnreadNotification(id, unread) {
     await this.props.setUnreadNotification(id, {
       unread
     });
-    await Promise.all([
-      this.props.getUnreadNotificationsList(),
-      this.props.getReadNotificationsList(),
-    ]);
+    await this.fetchNotifications();
+  }
+
+  async onRefresh() {
+    this.setState({
+      refreshing: true,
+    });
+    await this.fetchNotifications();
+    this.setState({
+      refreshing: false,
+    })
   }
 
   render () {
-    const { fetching, unreadNotifications, readNotifications } = this.props;
+    const {
+      fetchingUnreadNotifications,
+      fetchingReadNotifications,
+      unreadNotifications,
+      readNotifications,
+      refreshing
+    } = this.state;
 
     return (
       <View activePanel="panel">
@@ -75,62 +107,73 @@ class Notifications extends React.Component {
             Уведомления
           </PanelHeader>
 
-          <Group title="Новые уведомления">
-            {fetching && (
-              <DivSpinner />
-            )}
-            <List>
-              { unreadNotifications.map(notification => (
-                <NotificationCell
-                  key={notification.id}
-                  notification={notification}
-                  buttonBefore={<Icon24Hide/>}
-                  buttonLabel={"Пометить прочитанным"}
-                  onButtonClick={
-                    () => this.handleSetUnreadNotification(notification.id, false)
-                  }
-                  onSenderClick={
-                    notification.target
-                      ? () => this.props.history.push(`/${notification.target.content_type}/${notification.target.id}`)
-                      : null
-                  }
-                />
-              ))}
+          <PullToRefresh onRefresh={this.onRefresh} isFetching={refreshing}>
+            <div>
+              <Group title="Новые уведомления">
+                {fetchingUnreadNotifications
+                  ? <DivSpinner />
+                  : (
+                    <List>
+                      { unreadNotifications.map(notification => (
+                        <NotificationCell
+                          key={notification.id}
+                          notification={notification}
+                          buttonBefore={<Icon24Hide/>}
+                          buttonLabel={"Пометить прочитанным"}
+                          onButtonClick={
+                            () => this.handleSetUnreadNotification(notification.id, false)
+                          }
+                          onSenderClick={
+                            notification.target
+                              ? () => this.props.history.push(`/${notification.target.content_type}/${notification.target.id}`)
+                              : null
+                          }
+                        />
+                      ))}
 
-              { unreadNotifications.length === 0 && (
-                <Div>Нет уведомлений</Div>
-              )}
-            </List>
-          </Group>
+                      { unreadNotifications.length === 0 && (
+                        <Div>Нет уведомлений</Div>
+                      )}
+                    </List>
+                  )
+                }
+              </Group>
 
-          <Group title="Просмотренные уведомления">
-            <List>
-              { readNotifications.map(notification => (
-                <NotificationCell
-                  key={notification.id}
-                  notification={notification}
-                  buttonBefore={<Icon24View/>}
-                  buttonLabel={"Пометить непрочитанным"}
-                  onButtonClick={
-                    () => this.handleSetUnreadNotification(notification.id, true)
-                  }
-                  onSenderClick={
-                    notification.target
-                      ? () => this.props.history.push(`/${notification.target.content_type}/${notification.target.id}`)
-                      : null
-                  }
-                />
-              ))}
+              <Group title="Просмотренные уведомления">
+                {fetchingReadNotifications
+                  ? <DivSpinner />
+                  : (
+                    <List>
+                      { readNotifications.map(notification => (
+                        <NotificationCell
+                          key={notification.id}
+                          notification={notification}
+                          buttonBefore={<Icon24View/>}
+                          buttonLabel={"Пометить непрочитанным"}
+                          onButtonClick={
+                            () => this.handleSetUnreadNotification(notification.id, true)
+                          }
+                          onSenderClick={
+                            notification.target
+                              ? () => this.props.history.push(`/${notification.target.content_type}/${notification.target.id}`)
+                              : null
+                          }
+                        />
+                      ))}
 
-              {readNotifications.length === 0 && (
-                <Div>Нет уведомлений</Div>
-              )}
-            </List>
-          </Group>
+                      {!fetchingReadNotifications && readNotifications.length === 0 && (
+                        <Div>Нет уведомлений</Div>
+                      )}
+                    </List>
+                  )
+                }
+              </Group>
+            </div>
+          </PullToRefresh>
         </Panel>
       </View>
     )
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Notifications);
+export default connect(null, mapDispatchToProps)(Notifications);
