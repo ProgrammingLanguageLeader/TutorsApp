0d7c5796337c0f5efdf1c449cf2d53795a29894f
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-10 21:01:07 +0300

    DivSpinner refactoring

--- vk-apps-frontend/src/components/DivSpinner.js
+++ vk-apps-frontend/src/components/DivSpinner.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Spinner from '@vkontakte/vkui/dist/components/Spinner/Spinner';
 
+const SpinnerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+`;
+
 export default () => (
-  <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-    <Spinner size="large" style={{ marginTop: 20 }} />
-  </div>
+  <SpinnerWrapper>
+    <Spinner size="large" />
+  </SpinnerWrapper>
 );
