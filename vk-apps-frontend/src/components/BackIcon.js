63c6d4961a502bf3ba1ae7f15db33552d7375e1b
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-10 16:44:18 +0300

    typo fix

--- vk-apps-frontend/src/components/BackIcon.js
+++ vk-apps-frontend/src/components/BackIcon.js
@@ -5,7 +5,7 @@ import { platform, IOS } from '@vkontakte/vkui/dist/lib/platform';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 
-const BackButton = () => {
+const BackIcon = () => {
   const osname = platform();
   if (osname === IOS) {
     return <Icon28ChevronBack />;
@@ -13,4 +13,4 @@ const BackButton = () => {
   return <Icon24Back />;
 };
 
-export default BackButton;
+export default BackIcon;
