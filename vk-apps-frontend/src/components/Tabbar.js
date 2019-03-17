64ff1021b29a0e042c84bdc5af07b0fc780e514a
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-17 06:22:50 +0300

    fetching unread notifications count in tabbar

--- vk-apps-frontend/src/components/Tabbar.js
+++ vk-apps-frontend/src/components/Tabbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 
 import Tabbar from '@vkontakte/vkui/dist/components/Tabbar/Tabbar';
@@ -12,48 +14,98 @@ import Icon28Menu from '@vkontakte/icons/dist/28/menu';
 
 import TabbarLink from 'vk-apps-frontend/components/TabbarLink';
 
-const MyTabbar = ({ location, hidden, currentUserId, unreadNotificationsCount = 0 }) => (
-  !hidden && (
-    <Tabbar>
-      <TabbarLink to={`/user/${currentUserId}`}>
-        <TabbarItem selected={location.pathname.startsWith('/user')}>
-          <Icon28User />
-        </TabbarItem>
-      </TabbarLink>
+import { notificationsActions } from 'vk-apps-frontend/actions/api';
 
-      <TabbarLink to='/vacancies'>
-        <TabbarItem
-          selected={
-            location.pathname.startsWith('/vacancy')
-            || location.pathname.startsWith('/vacancies')
-          }
-        >
-          <Icon28Search />
-        </TabbarItem>
-      </TabbarLink>
+const mapDispatchToProps = dispatch => {
+  return {
+    getUnreadNotificationsList: bindActionCreators(notificationsActions.getUnreadNotificationsList, dispatch),
+  }
+};
 
-      <TabbarLink to='/schedule'>
-        <TabbarItem selected={location.pathname.startsWith('/schedule')}>
-          <Icon28Newsfeed />
-        </TabbarItem>
-      </TabbarLink>
+class MyTabbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      unreadNotificationsCount: 0,
+    };
+    this._isMounted = false;
+    this.notificationsInterval = null;
+  }
 
-      <TabbarLink to='/notifications'>
-        <TabbarItem
-          selected={location.pathname.startsWith('/notifications')}
-          label={unreadNotificationsCount ? unreadNotificationsCount : null}
-        >
-          <Icon28Notification/>
-        </TabbarItem>
-      </TabbarLink>
+  componentDidMount() {
+    this._isMounted = true;
+    this.getUnreadNotificationsListWithInterval.call(this);
+  }
 
-      <TabbarLink to='/main_menu'>
-        <TabbarItem selected={location.pathname.startsWith('/main_menu')}>
-          <Icon28Menu />
-        </TabbarItem>
-      </TabbarLink>
-    </Tabbar>
-  )
-);
+  componentWillUnmount() {
+    this._isMounted = false;
+    clearInterval(this.notificationsInterval);
+  }
 
-export default withRouter(MyTabbar);
+  async fetchUnreadNotificationsCount() {
+    const unreadNotificationsListResponse = await this.props.getUnreadNotificationsList();
+    const unreadNotificationsCount = unreadNotificationsListResponse.status === 200
+      && unreadNotificationsListResponse.data.count;
+    this._isMounted && this.setState({
+      unreadNotificationsCount
+    });
+  }
+
+  getUnreadNotificationsListWithInterval(interval = 30000) {
+    this.fetchUnreadNotificationsCount.call(this);
+    this.notificationsInterval = setInterval(() => {
+      this.fetchUnreadNotificationsCount.call(this);
+    }, interval);
+  }
+
+  render() {
+    const { location, hidden, currentUserId } = this.props;
+    const { unreadNotificationsCount } = this.state;
+
+    return (
+      !hidden && (
+        <Tabbar>
+          <TabbarLink to={`/user/${currentUserId}`}>
+            <TabbarItem selected={location.pathname.startsWith('/user')}>
+              <Icon28User/>
+            </TabbarItem>
+          </TabbarLink>
+
+          <TabbarLink to='/vacancies'>
+            <TabbarItem
+              selected={
+                location.pathname.startsWith('/vacancy')
+                || location.pathname.startsWith('/vacancies')
+              }
+            >
+              <Icon28Search/>
+            </TabbarItem>
+          </TabbarLink>
+
+          <TabbarLink to='/schedule'>
+            <TabbarItem selected={location.pathname.startsWith('/schedule')}>
+              <Icon28Newsfeed/>
+            </TabbarItem>
+          </TabbarLink>
+
+          <TabbarLink to='/notifications'>
+            <TabbarItem
+              selected={location.pathname.startsWith('/notifications')}
+              label={unreadNotificationsCount ? unreadNotificationsCount : null}
+            >
+              <Icon28Notification/>
+            </TabbarItem>
+          </TabbarLink>
+
+          <TabbarLink to='/main_menu'>
+            <TabbarItem selected={location.pathname.startsWith('/main_menu')}>
+              <Icon28Menu/>
+            </TabbarItem>
+          </TabbarLink>
+        </Tabbar>
+      )
+    );
+  }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(MyTabbar));
