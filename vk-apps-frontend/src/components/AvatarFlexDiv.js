3b7ae18a460e0756c93e88b0629d3cd20f60b518
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-19 01:25:31 +0300

    new styled components

---
+++ vk-apps-frontend/src/components/AvatarFlexDiv.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const AvatarFlexDiv = styled.div`
+  display: flex;
+  flex: 0 0 64px;
+`;
+
+export default AvatarFlexDiv;
---
+++ vk-apps-frontend/src/components/CellHeaderDiv.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const CellHeaderDiv = styled.div`
+  padding: 8px 11px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
+export default CellHeaderDiv;
