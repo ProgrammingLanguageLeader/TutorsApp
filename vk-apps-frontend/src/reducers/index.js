f334f579768097e04654db7631ea4a5f52c593a6
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-18 03:04:09 +0300

    unused old code cleanup

--- vk-apps-frontend/src/reducers/index.js
+++ vk-apps-frontend/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 
-import apiReducer from './api';
 import vkReducer from './vk';
 import vacanciesFilterReducer from './vacanciesFilterReducer';
 import currentUserReducer from './currentUserReducer';
 
 const rootReducer = combineReducers({
-  API: apiReducer,
   VK: vkReducer,
   vacanciesFilter: vacanciesFilterReducer,
   currentUser: currentUserReducer,
