c07bb449cdf547e5e85c00147b6a414bec4b6861
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-22 17:16:55 +0300

    cross platform animations

--- vk-apps-frontend/src/components/Root.js
+++ vk-apps-frontend/src/components/Root.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
+import { platform, IOS } from '@vkontakte/vkui/dist/lib/platform';
+
 const Root = ({ id, activeView, isBack, match, children }) => {
+  const currentPlatform = platform();
+  const transitionTimeout = currentPlatform === IOS ? 600 : 300;
   return (
     <ReactCSSTransitionGroup
       transitionName={isBack ? "view-back" : "view"}
-      transitionEnterTimeout={500}
-      transitionLeaveTimeout={500}
+      transitionEnterTimeout={transitionTimeout}
+      transitionLeaveTimeout={transitionTimeout}
     >
       {children.find(child => child.props.id === activeView)}
     </ReactCSSTransitionGroup>
