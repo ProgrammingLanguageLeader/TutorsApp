110c59d6bd55a63126e704eb706e17cfae1d09d3
Dmitry Shorokhov <vip.shoroch@gmail.com>
2019-03-27 05:45:16 +0300

    Animations improvement

--- vk-apps-frontend/src/components/Root.js
+++ vk-apps-frontend/src/components/Root.js
@@ -5,12 +5,13 @@ import { platform, IOS } from '@vkontakte/vkui/dist/lib/platform';
 
 const Root = ({ id, activeView, isBack, match, children }) => {
   const currentPlatform = platform();
-  const transitionTimeout = currentPlatform === IOS ? 600 : 500;
+  const transitionEnterTimeout = currentPlatform === IOS ? 800 : 400;
+  const transitionLeaveTimeout = currentPlatform === IOS ? 900 : 500;
   return (
     <ReactCSSTransitionGroup
       transitionName={isBack ? "view-back" : "view"}
-      transitionEnterTimeout={transitionTimeout}
-      transitionLeaveTimeout={transitionTimeout}
+      transitionEnterTimeout={transitionEnterTimeout}
+      transitionLeaveTimeout={transitionLeaveTimeout}
     >
       {children.find(child => child.props.id === activeView)}
     </ReactCSSTransitionGroup>
